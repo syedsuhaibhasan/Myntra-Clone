64778596ac3d85077926e2f6380fa008d7f46bbf
syedsuhaibhasan <syedsuhaibhasan888@gmail.com>
2026-04-08 02:54:50 +0500

    commit

--- bag.js
+++ bag.js
@@ -1,33 +1,55 @@
+let bagItemObjs;
 onLoad();
 function onLoad(){
+    loadBagItemsObjs();
     displayBagItems();
+    generateItemHtml();
 }
 
 function displayBagItems(){
     let ContainerElement = document.querySelector(".bag-items-container"); 
-    ContainerElement.innerHTML = `
+    let innerHTML = '';
+    bagItemObjs.forEach(bagItem => {
+        innerHTML += generateItemHtml(bagItem);
+    });
+    ContainerElement.innerHTML = innerHTML;
+}
+
+function loadBagItemsObjs(){
+    bagItemObjs = bagItems.map(itemId => {
+        for (let i = 0; i < item.length; i++) {
+            if(itemId == item[i].id){
+                return item[i];
+            }
+        }
+    });
+    console.log(bagItemObjs);
+}
+
+function generateItemHtml(item){
+     return `
          <div class="bag-item-container">
             <div class="item-left-part">
-              <img class="bag-item-img" src="../images/4.jpg">
+              <img class="bag-item-img" src="./${item.image}">
             </div>
             <div class="item-right-part">
-              <div class="company">ADIDAS</div>
-              <div class="item-name">Men Printed Polo Collar Indian Cricket ODI Jersey</div>
+              <div class="company">${item.company}</div>
+              <div class="item-name">${item.item_name}</div>
               <div class="price-container">
-                <span class="current-price">Rs 999</span>
-                <span class="original-price">Rs 999</span>
-                <span class="discount-percentage">(0% OFF)</span>
+                <span class="current-price">Rs ${item.current_price}</span>
+                <span class="original-price">Rs ${item.original_price}</span>
+                <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
               </div>
-              <div class="return-period">
+              <div class="${item.return_period}">
                 <span class="return-period-days">14 days</span> return available
               </div>
               <div class="delivery-details">
                 Delivery by
-                <span class="delivery-details-days">10 Oct 2023</span>
+                <span class="delivery-details-days">${item.delivery_date}</span>
               </div>
             </div>
 
             <div class="remove-from-cart">X</div>
           </div>
-    `
+    `;
 }
